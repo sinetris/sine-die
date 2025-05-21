d5158aa66df0e905091525c41d08bdfbb4a6b67f
Duilio Ruggiero <duilio@sinetris.info>
2025-05-21 16:05:08 +0200

    Move light/dark theme colors in scss file

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,4 +1,13 @@
 /** @type {import('stylelint').Config} */
 export default {
-  extends: ["stylelint-config-standard"]
+  extends: ["stylelint-config-standard"],
+  rules: {
+    "no-descending-specificity": null,
+  },
+  overrides: [
+    {
+      files: ["*.scss"],
+      extends: ["stylelint-config-standard-scss"],
+    },
+  ],
 };
